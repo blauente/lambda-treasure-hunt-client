43b00b316f66785eb2af0541f5afac577bbe8606
Grant Reighard <grant.reighard@gmail.com>
2019-01-29 21:42:15 -0500

    Added graph entry for previous room when moving.

--- client/src/components/Screen.js
+++ client/src/components/Screen.js
@@ -11,13 +11,15 @@ class Screen extends Component {
             input: '',
             currentRoom: '',
             prevRoom: '',
+            prevID: 0,
             currentDesc: '',
             currentPlayers: [],
             currentInv: '',
             cooldown: 0,
             traversing: false,
             coords: (0,0),
-            exits: []
+            exits: [],
+            room_id: 0
         }
     }
 
@@ -27,7 +29,7 @@ class Screen extends Component {
             {headers: {Authorization: 'Token 1ca713aa4805a973344e3bee86f39961d50927b8'}})
             .then(response => {
                 console.log(response.data)
-                this.setState(function () {return {currentRoom: response.data.title, currentDesc: response.data.description, cooldown: response.data.cooldown, coords: response.data.coordinates, exits: response.data.exits,
+                this.setState(function () {return {currentRoom: response.data.title, currentDesc: response.data.description, cooldown: response.data.cooldown, coords: response.data.coordinates, exits: response.data.exits, room_id: response.data.room_id,
                 movesLog: [...this.state.movesLog, 
                     {title: response.data.title,
                     description: response.data.description,
@@ -73,7 +75,7 @@ class Screen extends Component {
             {headers: {Authorization: 'Token 1ca713aa4805a973344e3bee86f39961d50927b8'}})
             .then(response => {
                 console.log(response.data)
-                this.setState({currentRoom: response.data.title, currentDesc: response.data.description, cooldown: response.data.cooldown, coords: response.data.coordinates, exits: response.data.exits,
+                this.setState({currentRoom: response.data.title, currentDesc: response.data.description, cooldown: response.data.cooldown, coords: response.data.coordinates, exits: response.data.exits, room_id: response.data.room_id,
                 movesLog: [...this.state.movesLog, 
                     {title: response.data.title,
                     description: response.data.description,
@@ -103,46 +105,54 @@ class Screen extends Component {
             
             this.interval2 = setInterval( () => {
 
-            graph[this.state.currentRoom] = {"coords": this.state.coords, "exits": ["?", "?", "?", "?"]};
-            console.log(graph);
+                graph[this.state.currentRoom] = {"coords": this.state.coords, "exits": ["?", "?", "?", "?"]};
+                console.log(graph);
 
-            const direction = this.state.exits[Math.floor(Math.random() * this.state.exits.length)];
-            let index = 0;
-            if (direction === "n") {
-                index = 0;
-            } else if (direction === "s") {
-                index = 1;
-            } else if (direction === "e") {
-                index = 2;
-            } else {
-                index = 3;
-            }
-            const data = {"direction": direction}
-            console.log("direction selected", direction);
-            this.setState(function () {return {prevRoom: this.state.currentRoom}})
+                const direction = this.state.exits[Math.floor(Math.random() * this.state.exits.length)];
+                let index = 0;
+                let oppIndex = 0;
+                if (direction === "n") {
+                    index = 0;
+                    oppIndex = 1;
+                } else if (direction === "s") {
+                    index = 1;
+                    oppIndex = 0;
+                } else if (direction === "e") {
+                    index = 2;
+                    oppIndex = 3;
+                } else {
+                    index = 3;
+                    oppIndex = 2;
+                }
+                const data = {"direction": direction}
+                console.log("direction selected", direction);
+                this.setState(function () {return {prevRoom: this.state.currentRoom, prevID: this.state.room_id}});
+                
 
-            axios
-            .post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', data, 
-            {headers: {Authorization: 'Token 1ca713aa4805a973344e3bee86f39961d50927b8'}})
-            .then(response => {
-                console.log(response.data)
-                this.setState(function () {return {currentRoom: response.data.title, currentDesc: response.data.description, cooldown: response.data.cooldown, coords: response.data.coordinates, exits: response.data.exits, 
-                movesLog: [...this.state.movesLog, 
-                    {title: response.data.title,
-                    description: response.data.description,
-                    players: response.data.players,
-                    room_id: response.data.room_id,
-                    cooldown: parseInt(response.data.cooldown),
-                    errors: response.data.errors}]}})
-                graph[this.state.prevRoom]["exits"][index] = response.data.room_id;
-                this.timeout2 = setTimeout(() => this.startBFT, this.state.cooldown * 1000);
-            })
-            .catch(error => {
-                console.log("ERROR:", error, Object.getOwnPropertyNames(error));
-                this.setState({ 
+                axios
+                .post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', data, 
+                {headers: {Authorization: 'Token 1ca713aa4805a973344e3bee86f39961d50927b8'}})
+                .then(response => {
+                    console.log(response.data)
+                    this.setState(function () {return {currentRoom: response.data.title, currentDesc: response.data.description, cooldown: response.data.cooldown, coords: response.data.coordinates, exits: response.data.exits, room_id: response.data.room_id, 
                     movesLog: [...this.state.movesLog, 
-                    {error: error.response.data.error}],
-                    cooldown: Math.round((parseFloat(error.response.data.cooldown)) * 100)/100})
+                        {title: response.data.title,
+                        description: response.data.description,
+                        players: response.data.players,
+                        room_id: response.data.room_id,
+                        cooldown: parseInt(response.data.cooldown),
+                        errors: response.data.errors}]}})
+                    graph[this.state.prevRoom]["exits"][index] = response.data.room_id;
+                    graph[response.data.title] = {"coords": this.state.coords, "exits": ["?", "?", "?", "?"]};
+                    graph[response.data.title]["exits"][oppIndex] = this.state.prevID;
+                    // this.timeout2 = setTimeout(() => this.startBFT, this.state.cooldown * 1000);
+                })
+                .catch(error => {
+                    console.log("ERROR:", error, Object.getOwnPropertyNames(error));
+                    this.setState({ 
+                        movesLog: [...this.state.movesLog, 
+                        {error: error.response.data.error}],
+                        cooldown: Math.round((parseFloat(error.response.data.cooldown)) * 100)/100})
             })}, this.state.cooldown * 1500);
 
             // this.interval = setInterval(() => {this.setState(function () {return {cooldown: this.state.cooldown - 1}})}, 1000);
