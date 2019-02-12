4f8377d5f420bd2c1be9427259df281ff7b11788
Grant Reighard <grant.reighard@gmail.com>
2019-02-12 13:35:27 -0500

    Added edges to map.

--- client/src/components/Map2.js
+++ client/src/components/Map2.js
@@ -44,41 +44,48 @@ class Map2 extends Component {
                 divs.push(<div className="mapNode" style={divStyle}>{keys[i].slice(5)}</div>)
                 for(let j = 0; j < exitKeys.length; j++) {
                     
-                    // if (exitKeys[j] === 'n') {
-                    //     context.moveTo(Number(coordinates[i][0])*40-2000+30, Number(coordinates[i][1])*40-2000-20)
-                    //     context.lineTo(Number(coordinates[i][0])*40-2000+10, Number(coordinates[i][1])*40-2000-20);
-                    //     context.strokeStyle = "#000";
-                    //     context.stroke();
-                    // } else if (exitKeys[j] === 's') {
-                    //     context.moveTo(Number(coordinates[i][0])*40-2000-30, Number(coordinates[i][1])*40-2000-20)
-                    //     context.lineTo(Number(coordinates[i][0])*40-2000-10, Number(coordinates[i][1])*40-2000-20);
-                    //     context.strokeStyle = "#000";
-                    //     context.stroke();
-                    // } else if (exitKeys[j] === 'e') {
-                    //     context.moveTo(Number(coordinates[i][0])*40-2000+20, Number(coordinates[i][1])*40-2000+30)
-                    //     context.lineTo(Number(coordinates[i][0])*40-2000+20, Number(coordinates[i][1])*40-2000+10);
-                    //     context.strokeStyle = "#000";
-                    //     context.stroke();
-                    // } else {
-                    //     context.moveTo(Number(coordinates[i][0])*40-2000-20, Number(coordinates[i][1])*40-2000-30)
-                    //     context.lineTo(Number(coordinates[i][0])*40-2000-20, Number(coordinates[i][1])*40-2000-10);
-                    //     context.strokeStyle = "#000";
-                    //     context.stroke();
-                    // }
+                    if (exitKeys[j] === 'n') {
+                        const edgeStyle= {
+                            marginTop: -(string.split(",")[1]*40-2000)+986,
+                            marginLeft: string.split(",")[0]*40-1990
+                        }
+                        divs.push(<div className="mapEdgeVert" style={edgeStyle}></div>)
+                    } else if (exitKeys[j] === 's') {
+                        const edgeStyle= {
+                            marginTop: -(string.split(",")[1]*40-2000)+1026,
+                            marginLeft: string.split(",")[0]*40-1990,
+                            // backgroundColor: 'red'
+                        }
+                        divs.push(<div className="mapEdgeVert" style={edgeStyle}></div>)
+                    } else if (exitKeys[j] === 'e') {
+                        const edgeStyle= {
+                            marginTop: -(string.split(",")[1]*40-2000)+1010,
+                            marginLeft: string.split(",")[0]*40-1974,
+                            // backgroundColor: 'red'
+                        }
+                        divs.push(<div className="mapEdgeHoriz" style={edgeStyle}></div>)
+                    } else {
+                        const edgeStyle= {
+                            marginTop: -(string.split(",")[1]*40-2000)+1010,
+                            marginLeft: string.split(",")[0]*40-2014,
+                            // backgroundColor: 'red'
+                        }
+                        divs.push(<div className="mapEdgeHoriz" style={edgeStyle}></div>)
+                    }
                     
                 }
 
             }
-            console.log("coordinates", coordinates);
-            for (let i = 0; i < coordinates.length; i++) {
-                let newKey = keys[i].slice(5);
-                // context.scale(1, -1);
+            // console.log("coordinates", coordinates);
+            // for (let i = 0; i < coordinates.length; i++) {
+            //     let newKey = keys[i].slice(5);
+            //     // context.scale(1, -1);
                 
-                // context.font = "12px Arial";
-                // context.fillText(newKey, Number(coordinates[i][0])*40-2000, Number(coordinates[i][1])*40-2000);
-                // // context.fillText("test", 500, 200)
-                // context.restore();
-            }
+            //     // context.font = "12px Arial";
+            //     // context.fillText(newKey, Number(coordinates[i][0])*40-2000, Number(coordinates[i][1])*40-2000);
+            //     // // context.fillText("test", 500, 200)
+            //     // context.restore();
+            // }
             return divs;
         }
 
